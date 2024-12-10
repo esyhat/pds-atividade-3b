51aa9f0dfe5cd406543b635980945c0916628db6
Thayse Feliciano Brito <thayse.feliciano@academico.ifrn.edu.br>
2024-12-10 15:33:53 -0300

    Projeto Pronto!

---
+++ Atividade/src/App.java
@@ -0,0 +1,14 @@
+
+public class App {
+	
+	 public static void main(String[] args) {
+	        // Criando uma instância de Pessoa para cada estuantes
+	        Pessoa pessoa = new Pessoa("Daniel", 25);
+	        Pessoa pessoa2 = new Pessoa("Thayse Feliciano Brito", 22);
+
+	        // Exibindo as informações dos estudantes
+	        pessoa.exibirInformacoes();
+	        pessoa2.exibirInformacoes();
+	    }
+
+}
